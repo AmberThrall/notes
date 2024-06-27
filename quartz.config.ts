0fb9c2a60ce2fa1ebc5c08cfb31ee43a24da0bf3
Amber Thrall <amber.rose.thrall@gmail.com>
2024-06-27 12:40:23 -0700

    fixed dates on articles

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "amber.thrall.me/notes/",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
